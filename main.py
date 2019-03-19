0e3766b8c2be71adfadce4021d75817498674c6a
KAFIA Youness <youness.kafia@soprasteria.com>
2019-03-19 18:23:38 +0100

    Second commit

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ a = data.groupby(["class"])["rawRedMean",
                             "rawGreenMean", "rawBlueMean"].median()
 a.index = classes
 ax = a.plot.bar(rot=0)
+fig = ax.get_figure().savefig("colorMeans.png")
 # Ce qui est bizarre : Il y a plus de vert que d'autres couleurs...
 # En y réfléchissant un peu plus, les images prises par des apareils photos munis d'un CCD ou d'un
 # CMOS utilisent un filtre de BAYER. La quantité de pixels vert est supérieur aux autres couleurs.
@@ -53,13 +54,14 @@ ax = a.plot.bar(rot=0)
 a = data.groupby(["class"])["exRedMean", "exGreenMean", "exBlueMean"].median()
 a.index = classes
 ax = a.plot.bar(rot=0)
+ax.get_figure().savefig("colorExcess.png")
 
 # %% Data preparation
-data['is_train'] = np.random.uniform(0, 1, len(data)) <= .75
-y_train, y_test = data.query("is_train == True")[
-    "class"], data.query("is_train == False")["class"]
-X_train, X_test = data.query("is_train == True").drop(["class", "is_train"], axis=1), data.query(
-    "is_train == False").drop(["class", "is_train"], axis=1)
+_data = data.copy()
+labels = _data["class"]
+_data['is_train'] = np.random.uniform(0, 1, len(data)) <= .75
+y_train, y_test =   _data.query("is_train == True")["class"], _data.query("is_train == False")["class"]
+X_train, X_test =   _data.query("is_train == True").drop(["class", "is_train"], axis=1), _data.query("is_train == False").drop(["class", "is_train"], axis=1)
 X_train.head()
 #data = data.drop(labels="class",axis=1)
 
@@ -68,29 +70,28 @@ X_train.head()
 
 
 n_samples = 1500
-random_state = 100
-maxScore = 0
-bestC = 0
-for i in range(5, 15):
-    y_pred = KMeans(
-        n_clusters=i, random_state=random_state).fit_predict(X_train)
-    score = metrics.adjusted_rand_score(y_train, y_pred)
-    maxScore, bestC = (score, i) if score > maxScore else (maxScore, bestC)
-    print("best = ", bestC)
-    print("best score = ", maxScore)
-    print(str(i)+" gave us a score of " + str(score))
-y_pred = KMeans(n_clusters=bestC,
-                random_state=random_state).fit_predict(X_train)
+random_state = 170
+
+y_pred = KMeans(n_clusters=7,
+                random_state=random_state).fit_predict(data)
+print(metrics.adjusted_rand_score(labels,y_pred))
 plt.subplot(221)
-plt.scatter(X_train.iloc[:, 0], X_train.iloc[:, 1], c=y_pred)
+plt.scatter(data.iloc[:, 0], data.iloc[:, 1], c=y_pred)
 plt.title("Clustering color areas based on their data")
+plt.savefig("Clustering.png")
 
-# %% Etape 4 : Logistic regression
-
-
+# %% Etape 4 : Logistic regression with 3 solvers
+results ={}
+model = LogisticRegression(random_state=0, solver='sag',
+                           multi_class='multinomial').fit(X_train, y_train)
+results["sag"] = model.score(X_test, y_test)
 model = LogisticRegression(random_state=0, solver='lbfgs',
                            multi_class='multinomial').fit(X_train, y_train)
-model.score(X_test, y_test)
+results["lbfgs"] = model.score(X_test, y_test)
+model = LogisticRegression(random_state=0, solver='newton-cg',
+                           multi_class='multinomial').fit(X_train, y_train)
+results["newton-cg"] = model.score(X_test, y_test)
+results
 
 
 # %% Etape 5 : Random Forest
@@ -101,7 +102,8 @@ Forest = RandomForestClassifier(n_estimators=100, max_depth=4,
 Forest.fit(X_train, y_train)
 feature_importances = pd.DataFrame(Forest.feature_importances_,
                                    index=X_train.columns,
-                                   columns=['importance']).sort_values('importance', ascending=False)
+                                   columns=['importance'])
+feature_importances = feature_importances.sort_values('importance', ascending=False)
 
 print(feature_importances)
 print("Overall accuracy for the test set : ", Forest.score(X_test, y_test))
@@ -111,8 +113,8 @@ print("Overall accuracy for the test set : ", Forest.score(X_test, y_test))
 from sklearn.model_selection import GridSearchCV
 
 param_grid = { 
-    'n_estimators': [200, 700],
-    'max_features': ['auto', 'sqrt', 'log2']
+    'n_estimators': [50, 600],
+    'max_features': ['sqrt', 'log2']
 }
 ForestToSearch = RandomForestClassifier(
                     n_estimators=100, max_depth=4,
@@ -121,15 +123,23 @@ gridForest = GridSearchCV(ForestToSearch,param_grid,cv=5)
 gridForest.fit(X_train, y_train)
 
 print(gridForest.best_params_)
-print("Overall accuracy for the test set : ", gridForest.score(X_test, y_test))
+print("Overall accuracy for the test set : ",
+     gridForest.score(X_test, y_test))
 
 #%% Etape 7 : Randomized Search
 
 from sklearn.model_selection import RandomizedSearchCV
+from random import randint
 
 ForestToSearch = RandomForestClassifier(
                     n_estimators=100, max_depth=4,
                     random_state=0)
-searchForest = GridSearchCV(ForestToSearch,param_grid,cv=5)
+param_dist = {"max_depth": [3, None],
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"]}
+searchForest = RandomizedSearchCV(ForestToSearch,param_dist,n_iter=8)
 searchForest.fit(X_train, y_train)
-
+print(searchForest.best_params_)
+#%%
+print("Overall accuracy for the test set : ",
+     gridForest.score(X_test, y_test))
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Project : Python for Data Analysis
+
+
+## Contexte
+Le contexte du projet est de fournir une analyse d'un dataset au travers de visualisations et de modélisations.
+Le dataset utilisé est un dataset de segments d'images labélisés.
+Ces segements représentent des éléments visuels comme :
+ * Le ciel
+ * Un feuillage
+ * De l'herbe
+ * etc
+
+## Objectifs
+
+La première étape de ce projet est de fournir une visualisation succinte sur le dataset afin de proposer quelques intuitions qui serviront à la modélisation.
+
+Dans une seconde étape, plusieurs modélisations seront faites. Ces modélisations seront étudié par une des modifications d'hyper-paramêtres.
